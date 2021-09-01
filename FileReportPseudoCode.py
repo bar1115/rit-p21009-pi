12349742c571c5bf8b2692fe7347d396bb9602cf
tsosa94 <89881251+tsosa94@users.noreply.github.com>
2021-08-31 20:07:04 -0400

    Add files via upload

---
+++ FileReportPseudoCode.py
@@ -0,0 +1,27 @@
+import machine
+import utime
+
+logfile = open("log.txt","w") "The Raspberry Pi will expect a file named log.txt from the NXG
+"""
+ReportFile will be created as a class with distinct attributes so that it may 
+"""
+class reportFile (self, fileName, avgAcc, currLoc, currAcc, comfort, jolt, impact):
+class reportFile:
+    def __init__(self, fileName, avgAcc, currLoc, currAcc, comfort, currTemp, jolt, impact):
+        self.fileName = fileName
+        self.avgAcc = exprirationDate
+        self.currAcc = currAcc 
+        self.currLoc = currLoc
+        self.comfort = TRUE
+        self.currTemp = currTemp
+        self.
+        reportFile = open("report.txt","w")
+
+for line in logfile:
+    if val.contains == accelerationTags:
+        Add to reportFile.acceleration
+    else val.contains == locationTags:
+        Add to repotFile.acceleration
+current_speed
+current_location
+
