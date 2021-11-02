d89e6bf16b40ac2d93a095680e0c65238fbd15b0
Rebecca Reich <bar1115@rit.edu>
2021-11-02 16:32:48 -0400

    Updated encoding/parsing methods to include status values. Began adding in error checking

--- LogData.py
+++ LogData.py
@@ -1,17 +1,21 @@
 class LogData(object):
 
-    def __init__(self, sensorType, location, dataType, data):
+    # Overloaded Constructor
+    #   - If no values are input, the LogData properties are 'None' 
+    def __init__(self, sensorType = None, location = None, dataType = None, dataLog = None, dataRaw = None, status = False):
 
         self.sensorType = sensorType
         self.location = location
         self.dataType = dataType
-        self.data = data
+        self.dataLog = dataLog
+        self.dataRaw = dataRaw
+        self.status = status
 
     def __str__(self):
-        return "Sensor Type: " + self.sensorType + ", Location: " + self.location  + ", Data Type: " + self.dataType + ", Data: " + self.data
+        return "Sensor Type: " + self.sensorType + ", Location: " + self.location  + ", Data Type: " + self.dataType + ", Data: " + self.dataLog
 
     def getData(self):
-        return self.sensorType, self.location, self.dataType, self.data
+        return self.sensorType, self.location, self.dataType, self.dataLog
 
     def getSensorType(self):
         return self.sensorType
@@ -22,8 +26,14 @@ class LogData(object):
     def getDataType(self):
         return self.dataType
     
-    def getData(self):
-        return self.data
+    def getDataLog(self):
+        return self.dataLog
+
+    def getDataRaw(self):
+        return self.dataRaw
+
+    def isStatusType(self):
+        return self.status
 
     
 
--- SystemLogging_TEST.py
+++ SystemLogging_TEST.py
@@ -7,28 +7,37 @@ testSysLog = SystemLogging()
 SystemLogging.createFolderStructure(testSysLog)
 
 # Create random encodings for all sensor cases
-testParseEncode0 = SystemLogging.parseEncoding(testSysLog, "OB>H>ACC>1234>4567>78910")
+# Data Encodings
+testParseEncode0 = SystemLogging.parseEncoding("OB>H>ACC>1234>4567>78910")
 print(testParseEncode0)
-testParseEncode1 = SystemLogging.parseEncoding(testSysLog, "OB>H>EUL>10987>7654>4321")
+testParseEncode1 = SystemLogging.parseEncoding("OB>H>EUL>10987>7654>4321")
 print(testParseEncode1)
-testParseEncode2 = SystemLogging.parseEncoding(testSysLog, "OB>B>ACC>ABC>EFG>HIJ")
+testParseEncode2 = SystemLogging.parseEncoding("OB>B>ACC>ABC>EFG>HIJ")
 print(testParseEncode2)
-testParseEncode3 = SystemLogging.parseEncoding(testSysLog, "SCAL>CH>FRC>40404040404")
+testParseEncode3 = SystemLogging.parseEncoding("SCAL>CH>FRC>40404040404")
 print(testParseEncode3)
-testParseEncode4 = SystemLogging.parseEncoding(testSysLog, "FSR>LR>FRC>12345678910")
-print(testParseEncode4)
-testParseEncode5 = SystemLogging.parseEncoding(testSysLog, "FSR>RR>FRC>12345678910")
-print(testParseEncode5)
-testParseEncode6 = SystemLogging.parseEncoding(testSysLog, "FSR>LF>FRC>12345678910")
-print(testParseEncode6)
-testParseEncode7 = SystemLogging.parseEncoding(testSysLog, "IMU>LL>ACC>ABC>ABC>ABC")
-print(testParseEncode7)
-testParseEncode8 = SystemLogging.parseEncoding(testSysLog, "IMU>LL>GYR>ABC>ABC>ABC")
-print(testParseEncode8)
-testParseEncode9 = SystemLogging.parseEncoding(testSysLog, "IMU>RL>ACC>ABC>ABC>ABC")
-print(testParseEncode9)
-testParseEncode10 = SystemLogging.parseEncoding(testSysLog, "IMU>RL>GYR>ABC>ABC>ABC")
-print(testParseEncode10)
+# testParseEncode4 = SystemLogging.parseEncoding("FSR>LR>FRC>12345678910")
+# print(testParseEncode4)
+# testParseEncode5 = SystemLogging.parseEncoding("FSR>RR>FRC>12345678910")
+# print(testParseEncode5)
+# testParseEncode6 = SystemLogging.parseEncoding("FSR>LF>FRC>12345678910")
+# print(testParseEncode6)
+# testParseEncode7 = SystemLogging.parseEncoding("IMU>LL>ACC>ABC>ABC>ABC")
+# print(testParseEncode7)
+# testParseEncode8 = SystemLogging.parseEncoding("IMU>LL>GYR>ABC>ABC>ABC")
+# print(testParseEncode8)
+# testParseEncode9 = SystemLogging.parseEncoding("IMU>RL>ACC>ABC>ABC>ABC")
+# print(testParseEncode9)
+# testParseEncode10 = SystemLogging.parseEncoding("IMU>RL>GYR>ABC>ABC>ABC")
+# print(testParseEncode10)
+
+# Status Encodings
+testParseEncode11 = SystemLogging.parseEncoding("SCAL>CH>EN>1")
+print(testParseEncode11)
+testParseEncode12 = SystemLogging.parseEncoding("OB>H>CAL>1>2>3>4")
+print(testParseEncode12)
+testParseEncode13 = SystemLogging.parseEncoding("SCAL>CH>EN>321")
+print(testParseEncode13)
 
 # Loop 1000 and populate each sensor type with faux encoding
 for i in range(0, 1000):
@@ -43,23 +52,32 @@ for i in range(0, 1000):
 for i in range(0, 1000):
     SystemLogging.populateLog( testSysLog, testParseEncode3)
 
-for i in range(0, 1000):
-    SystemLogging.populateLog( testSysLog, testParseEncode4)
+# for i in range(0, 1000):
+#     SystemLogging.populateLog( testSysLog, testParseEncode4)
 
-for i in range(0, 1000):
-    SystemLogging.populateLog( testSysLog, testParseEncode5)
+# for i in range(0, 1000):
+#     SystemLogging.populateLog( testSysLog, testParseEncode5)
 
-for i in range(0, 1000):
-    SystemLogging.populateLog( testSysLog, testParseEncode6)
+# for i in range(0, 1000):
+#     SystemLogging.populateLog( testSysLog, testParseEncode6)
 
-for i in range(0, 1000):
-    SystemLogging.populateLog( testSysLog, testParseEncode7)
+# for i in range(0, 1000):
+#     SystemLogging.populateLog( testSysLog, testParseEncode7)
 
-for i in range(0, 1000):
-    SystemLogging.populateLog( testSysLog, testParseEncode8)
+# for i in range(0, 1000):
+#     SystemLogging.populateLog( testSysLog, testParseEncode8)
 
-for i in range(0, 1000):
-    SystemLogging.populateLog( testSysLog, testParseEncode9)
+# for i in range(0, 1000):
+#     SystemLogging.populateLog( testSysLog, testParseEncode9)
 
-for i in range(0, 1000):
-    SystemLogging.populateLog( testSysLog, testParseEncode10)
+# for i in range(0, 1000):
+#     SystemLogging.populateLog( testSysLog, testParseEncode10)
+
+for i in range(0, 100):
+    SystemLogging.populateStatus( testSysLog, testParseEncode11)
+
+for i in range(0, 100):
+    SystemLogging.populateStatus( testSysLog, testParseEncode12)
+
+for i in range(0, 100):
+    SystemLogging.populateStatus( testSysLog, testParseEncode13)
