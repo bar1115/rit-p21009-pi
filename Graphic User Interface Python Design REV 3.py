1bf573775751a99cb2a276a1b4448cca06b08e1d
Rebecca Reich <bar1115@rit.edu>
2021-11-10 15:13:47 -0500

    Updated layout of Thomas' GUI
    I have gone through and beautified the layout of Thomas' origional
    GUI. Upon further inspection, it seems the current model is not
    extensive enough to meet our interface needs. This model needs
    heavy edits with mutliple screen togglings to appropriatley handle
    the UI.

--- Graphic User Interface Python Design REV 3.py
+++ Graphic User Interface Python Design REV 3.py
@@ -1,132 +1,122 @@
 import tkinter as tk
 import tkinter.font as tkFont
+from tkinter import *
+from PIL import Image, ImageTk
 
 class UserInterface:
+
     def __init__(self, root):
-        #setting title
+        """
+        Initialize the Buttons and Label graphics
+
+        Args:
+            root (Tk): The tkinter object necessary to estable layout
+        """
+
+        # Setting - Title
         root.title("undefined")
-        #setting window size
-        width=600
-        height=500
+
+        # Setting - Window Size
+        width=480
+        height=320
+        highlightColor = "#85d3e9"
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
         alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
         root.geometry(alignstr)
         root.resizable(width=False, height=False)
 
-        GLabel_293=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=10)
-        GLabel_293["font"] = ft
-        GLabel_293["fg"] = "#333333"
-        GLabel_293["justify"] = "center"
-        GLabel_293["text"] = "Footer"
-        GLabel_293.place(x=0,y=410,width=600,height=86)
+        # TASK BAR BAND
+        bottomBar=tk.Label(root)
+        bottomBar["bg"] = highlightColor
+        bottomBar.pack( ipady=12, side='bottom', fill='x' )
 
-        GLabel_476=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=10)
-        GLabel_476["font"] = ft
-        GLabel_476["fg"] = "#333333"
-        GLabel_476["justify"] = "center"
-        GLabel_476["text"] = "Header"
-        GLabel_476.place(x=0,y=60,width=600,height=59)
+        # START TESTING BUTTON
+        start = Image.open("icons\play.png")
+        start = start.resize((25, 25), Image.ANTIALIAS)
+        start = ImageTk.PhotoImage(start)       
+        startButton=tk.Button(root, bg=highlightColor, bd=0, image=start)
+        startButton.image = start
+        startButton.place(x=10,y=280,width=50,height=40)
+        startButton["command"] = self.startEvent
 
-        GButton_442=tk.Button(root)
-        GButton_442["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_442["font"] = ft
-        GButton_442["fg"] = "#000000"
-        GButton_442["justify"] = "center"
-        GButton_442["text"] = "Start"
-        GButton_442.place(x=10,y=450,width=92,height=43)
-        GButton_442["command"] = self.GButton_442_command
+        # STOP TESTING BUTTON
+        stop = Image.open("icons\pause.png")
+        stop = stop.resize((25, 25), Image.ANTIALIAS)
+        stop = ImageTk.PhotoImage(stop)       
+        stopButton=tk.Button(root, bg=highlightColor, bd=0, image=stop)
+        stopButton.image = stop
+        stopButton.place(x=100,y=280,width=50,height=40)
+        stopButton["command"] = self.stopEvent
 
-        GLabel_634=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=10)
-        GLabel_634["font"] = ft
-        GLabel_634["fg"] = "#333333"
-        GLabel_634["justify"] = "center"
-        GLabel_634["text"] = "Body of the Display"
-        GLabel_634.place(x=250,y=210,width=150,height=25)
+        # SAVE DATA TO USB
+        save = Image.open("icons\save.png")
+        save = save.resize((25, 25), Image.ANTIALIAS)
+        save = ImageTk.PhotoImage(save)
+        saveButton=tk.Button(root, bg=highlightColor, bd=0, image=save)
+        saveButton.image = save
+        saveButton.place(x=330,y=280,width=50,height=40)
+        saveButton["command"] = self.saveButton_command
 
-        GButton_215=tk.Button(root)
-        GButton_215["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_215["font"] = ft
-        GButton_215["fg"] = "#000000"
-        GButton_215["justify"] = "center"
-        GButton_215["text"] = "Stop"
-        GButton_215.place(x=130,y=450,width=92,height=42)
-        GButton_215["command"] = self.GButton_215_command
+        # 
+        send = Image.open("icons\send.png")
+        send = send.resize((25, 25), Image.ANTIALIAS)
+        send = ImageTk.PhotoImage(send)
+        sendButton=tk.Button(root, bg=highlightColor, bd=0, image=send)
+        sendButton.image = send
+        sendButton.place(x=420,y=280,width=50,height=40)
+        sendButton["command"] = self.sendEvent
 
-        GButton_844=tk.Button(root)
-        GButton_844["bg"] = "#efefef"
+        dataButton=tk.Button(root)
+        dataButton["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=10)
-        GButton_844["font"] = ft
-        GButton_844["fg"] = "#000000"
-        GButton_844["justify"] = "center"
-        GButton_844["text"] = "Save"
-        GButton_844.place(x=360,y=450,width=92,height=44)
-        GButton_844["command"] = self.GButton_844_command
+        dataButton["font"] = ft
+        dataButton["fg"] = "#000000"
+        dataButton["justify"] = "center"
+        dataButton["text"] = "Data"
+        dataButton.place(x=140,y=10,width=82,height=30)
+        dataButton["command"] = self.dataEvent
 
-        GButton_0=tk.Button(root)
-        GButton_0["bg"] = "#efefef"
+        reportButton=tk.Button(root)
+        reportButton["bg"] = "#efefef"
         ft = tkFont.Font(family='Times',size=10)
-        GButton_0["font"] = ft
-        GButton_0["fg"] = "#000000"
-        GButton_0["justify"] = "center"
-        GButton_0["text"] = "Send"
-        GButton_0.place(x=480,y=450,width=92,height=44)
-        GButton_0["command"] = self.GButton_0_command
+        reportButton["font"] = ft
+        reportButton["fg"] = "#000000"
+        reportButton["justify"] = "center"
+        reportButton["text"] = "Report"
+        reportButton.place(x=250,y=10,width=75,height=30)
+        reportButton["command"] = self.reportEvent
 
-        GMessage_430=tk.Message(root)
-        ft = tkFont.Font(family='Times',size=10)
-        GMessage_430["font"] = ft
-        GMessage_430["fg"] = "#333333"
-        GMessage_430["justify"] = "center"
-        GMessage_430["text"] = "Livability Lab Logo"
-        GMessage_430.place(x=0,y=20,width=80,height=25)
+        logo = Image.open("icons/logo.png")
+        logo = logo.resize((115, 35), Image.ANTIALIAS)
+        logo = ImageTk.PhotoImage(logo)
+        logoLabel = Label(root, image=logo)
+        logoLabel.image = logo
+        #logoLabel.pack()
+        logoLabel.place(x = 5 , y = 5)
+        
 
-        GButton_461=tk.Button(root)
-        GButton_461["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_461["font"] = ft
-        GButton_461["fg"] = "#000000"
-        GButton_461["justify"] = "center"
-        GButton_461["text"] = "Data"
-        GButton_461.place(x=110,y=10,width=82,height=30)
-        GButton_461["command"] = self.GButton_461_command
-
-        GButton_771=tk.Button(root)
-        GButton_771["bg"] = "#efefef"
-        ft = tkFont.Font(family='Times',size=10)
-        GButton_771["font"] = ft
-        GButton_771["fg"] = "#000000"
-        GButton_771["justify"] = "center"
-        GButton_771["text"] = "Report"
-        GButton_771.place(x=190,y=10,width=75,height=30)
-        GButton_771["command"] = self.GButton_771_command
-
-    def GButton_442_command(self):
+    def startEvent(self):
         print("start collecting data")
 
 
-    def GButton_215_command(self):
+    def stopEvent(self):
         print("stop collecting data")
 
 
-    def GButton_844_command(self):
+    def saveButton_command(self):
         print("save data")
 
 
-    def GButton_0_command(self):
+    def sendEvent(self):
         print("send data")
 
 
-    def GButton_461_command(self):
+    def dataEvent(self):
         print("This activates the data tab")
 
 
-    def GButton_771_command(self):
+    def reportEvent(self):
         print("This activates the report tab")
 
 if __name__ == "__main__":
